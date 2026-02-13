f6dc86a4862e2ba3a800ea24aebe362de061c3a6
BG <biljan.georgiev@hotmail.com>
2026-02-13 18:22:09 +0100

    Fix duplicate

--- src/main/java/inf222/aop/account/aspect/TransferAspect.java
+++ src/main/java/inf222/aop/account/aspect/TransferAspect.java
@@ -15,7 +15,7 @@ import inf222.aop.account.annotation.Transfer;
 @Aspect
 public class TransferAspect {
 
-    @Around("@annotation(transferAnnotation)")    // intercepts any method annotated with @Transfer.
+    @Around("execution(* *(..)) && @annotation(transferAnnotation)")    // intercepts any method annotated with @Transfer. execution(...) restricts it strictly to method execution.
     public Object logTransfer(ProceedingJoinPoint pjp, Transfer transferAnnotation) throws Throwable {
 
         Object result = pjp.proceed();  //Call the Original Method
